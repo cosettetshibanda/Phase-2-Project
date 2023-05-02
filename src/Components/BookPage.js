fcc2b98d5814c2bf8b735bd12aa5627449a2a600
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-05-02 01:13:38 -0600

    deleted filter as it didn't work

--- src/Components/BookPage.js
+++ src/Components/BookPage.js
@@ -5,42 +5,9 @@ import Search from "./Search"
 
 function Home ({ onDeleteBook, search, setSearch, booksToDisplay }){
 
-    const [sortBy, setSortBy] = useState ("")
-
-    const sortBooks = (e) => {
-        setSortBy(e.target.value)
-    }
-
-    useEffect(()=> {
-        if(sortBy === 'Alphabetically'){
-            const sortedBooks = sortByName()
-            setSearch(sortedBooks)
-        }else{
-
-        }
-    }, [sortBy])
-
-    const sortByName = () => {
-        return [...booksToDisplay].sort((a, b) => {
-            const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-            const nameB = b.name.toUpperCase(); // ignore upper and lowercase
-            if (nameA < nameB) {
-              return -1;
-            }
-            if (nameA > nameB) {
-              return 1;
-            }
-            return 0;
-          });
-    }
-
-    const sortByYear = () => {
-
-    }
-
     return (
         <div className="home">
-            <Search sortBooks={sortBooks} search={search} setSearch={setSearch}  />
+            <Search search={search} setSearch={setSearch}  />
             <BooksContainer books={booksToDisplay} onDeleteBook={onDeleteBook} />
         </div>
     )
