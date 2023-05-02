400ab4f15a1ddb05c4136c257a1ddb7640cc513c
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-05-02 01:11:31 -0600

    attempt to add a filter

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,4 @@
-function Search ({ search, setSearch }){
+function Search ({ sortBooks, sortBy, search, setSearch }){
     function handleSearch(event){
         setSearch(event.target.value)
     }
@@ -8,6 +8,29 @@ function Search ({ search, setSearch }){
             <div className="input">
                 <h3>Search</h3>
                 <input className="prompt" value={search} onChange={handleSearch} />
+            </div>
+            <div >
+            <strong>Sort by:</strong>
+      <label>
+        <input
+          type="radio"
+          value="Alphabetically"
+          name="sort"
+        //   checked={sortBy === "Alphabetically"}
+          onChange={sortBooks}
+        />
+        Alphabetically
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Year"
+          name="sort"
+        //   checked={sortBy === "Year"}
+          onChange={sortBooks}
+        />
+        Year Published
+      </label>
             </div>
         </div>
     )
